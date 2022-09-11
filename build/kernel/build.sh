5d67a26247b9fafa086748dd2dcec5ef20e1110c
Daan Henke <daan@daan.vodka>
2022-09-11 15:11:18 +0200

    Updated container

--- build/kernel/build.sh
+++ build/kernel/build.sh
@@ -1,2 +1,2 @@
-echo Commit hash: $SKYLINE_COMMIT_HASH, Tag: $SKYLINE_TAG, Arch: $SKYLINE_ARCH
+echo Commit hash: $SKYLINE_COMMIT_HASH, Tag: $SKYLINE_TAG, Arch: $SKYLINE_ARCH, Subarch: $SKYLINE_SUB_ARCH, Bootloader: $SKYLINE_BOOTLOADER
 ls /code/kernel
